a1688c1fd5b44dfae84b066cb4403b7915b6f3a3
unknown <juniordomingos738@gmail.com>
2023-03-28 11:15:07 -0300

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const Api = axios.create({
     // URL da Api de Autenticação //
-    baseURL: "http://127.0.0.1/api/",//"http://164.90.255.137/api/",
+    baseURL: "http://apitradersdash.infinityfreeapp.com/api/",//"http://164.90.255.137/api/",
 });
